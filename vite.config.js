134d8cd17945ed364350ef52c0955bd943093c5f
Schmidt-avanti <aa.schmidt@ja-dialog.de>
2025-11-12 12:44:15 +0100

    chore: disable minification in production build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: './',
   build: {
+      minify: false,
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
